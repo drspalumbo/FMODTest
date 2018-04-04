9b74cc324856479456794a76acae6ef729fe7ed0
Dave Palumbo (HomePC) <drspalumbo@gmail.com>
2018-04-03 22:12:09 -0400

    First Commit.

---
+++ FMODTestProject_01/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody rb;
+	public float moveForce = 100f;
+	public float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.A)){
+			rb.AddForce(-1 * moveForce, 0, 0) ;
+		}
+		if (Input.GetKey(KeyCode.S)){
+			rb.AddForce(0, 0, -1 * moveForce);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			rb.AddForce(1 * moveForce, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.W)){
+			rb.AddForce(0, 0, 1 * moveForce);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(0, 1 * jumpForce,0);
+		}
+
+	}
+}
